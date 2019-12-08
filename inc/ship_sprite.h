b1d5c5a6c24908973c4e85f4acc5a2038de59ad4
I501975 <bruno.souza@sap.com>
2019-12-07 23:07:46 -0300

    add ship_sprite draft

---
+++ inc/ship_sprite.h
@@ -0,0 +1,20 @@
+#ifndef SHIP_SPRITE_H_INCLUDED
+#define SHIP_SPRITE_H_INCLUDED
+#include <timer.h>
+#include <tile_texture.h>
+#include <string>
+class ship_sprite
+{
+private:
+    tile_texture tex;
+    unsigned int currentAction;
+    float period;
+
+public:
+    ship_sprite(std::string path, int frameQtd, int actionQtd, float width, float height, float period, timer *t);
+    void bindAction(unsigned int action);
+    void draw(NDC pos);
+    ~ship_sprite(){};
+};
+
+#endif
